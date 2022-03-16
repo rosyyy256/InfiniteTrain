efedc322d691cab89790c7726019b95ed71deeb7
rosyyy <rosyyy2101@gmail.com>
2022-03-16 22:53:15 +0400

    Final result

--- InfiniteTrain/RailwayCarriage.cs
+++ InfiniteTrain/RailwayCarriage.cs
@@ -2,6 +2,26 @@
 {
     public class RailwayCarriage
     {
-        
+        public int Number;
+        public RailwayCarriage FrontCarriage { get; private set; }
+        public RailwayCarriage RearCarriage { get; private set; }
+        public bool IsLightsOn { get; private set; }
+
+        public RailwayCarriage(bool isLightsOn)
+        {
+            IsLightsOn = isLightsOn;
+        }
+
+        public void SwitchLights() => IsLightsOn = !IsLightsOn;
+
+        public void SetFrontCarriage(RailwayCarriage carriage)
+        {
+            FrontCarriage = carriage;
+        }
+
+        public void SetRearCarriage(RailwayCarriage carriage)
+        {
+            RearCarriage = carriage;
+        }
     }
 }
---
+++ InfiniteTrain/Train.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace InfiniteTrain
+{
+    public class Train
+    {
+        public RailwayCarriage Carriage { get; private set; }
+
+        public Train()
+        {
+            var startCarriage = new RailwayCarriage(GetRandomBool());
+            var frontCarriage = new RailwayCarriage(GetRandomBool());
+            var rearCarriage = new RailwayCarriage(GetRandomBool());
+            startCarriage.SetFrontCarriage(frontCarriage);
+            startCarriage.SetRearCarriage(rearCarriage);
+            frontCarriage.SetFrontCarriage(rearCarriage);
+            frontCarriage.SetRearCarriage(startCarriage);
+            rearCarriage.SetFrontCarriage(startCarriage);
+            rearCarriage.SetRearCarriage(frontCarriage);
+            Carriage = startCarriage;
+        }
+
+        public Train(int carriagesCount) : this()
+        {
+            if (carriagesCount < 3) throw new ArgumentException("Carriages count must be more than 3");
+            carriagesCount -= 3;
+
+            for (var i = 0; i < carriagesCount; i++)
+            {
+                var currentCarriage = new RailwayCarriage(GetRandomBool());
+                currentCarriage.SetFrontCarriage(Carriage.FrontCarriage);
+                currentCarriage.SetRearCarriage(Carriage);
+                Carriage.FrontCarriage.SetRearCarriage(currentCarriage);
+                Carriage.SetFrontCarriage(currentCarriage);
+            }
+        }
+
+        public int CountCarriages()
+        {
+            var currentCarriage = Carriage;
+            if (!currentCarriage.IsLightsOn) currentCarriage.SwitchLights();
+
+            while (true)
+            {
+                var result = 0;
+                while (!currentCarriage.FrontCarriage.IsLightsOn)
+                {
+                    currentCarriage = currentCarriage.FrontCarriage;
+                    result++;
+                }
+                currentCarriage.FrontCarriage.SwitchLights();
+                for (var i = 0; i < result; i++)
+                {
+                    currentCarriage = currentCarriage.RearCarriage;
+                }
+
+                if (!currentCarriage.IsLightsOn) return result + 1;
+            }
+        }
+
+        private static bool GetRandomBool()
+        {
+            var random = new Random();
+            return random.Next(0, 9) < 5;
+        }
+    }
+}
